2464212fd5e590a51af721d86e761f7bed2896fc
anrinoorahim <noorahim19@gmail.com>
2023-05-19 06:35:42 +0700

    Model dan Controller

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rules\File;
+use Illuminate\Support\Facades\Storage;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $data['category'] = category::all();
+        return view('category.index', $data);
+    }
+
+
+        /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validasi = $request->validate([
+            'category_name' => 'required',
+            'description' => 'required',
+            'photo' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:10'
+        ]);
+
+        if ($request->file('photo')) {
+            $validasi['photo'] = $request->file('photo')->store('gambar');
+        }
+
+        category::create($validasi);
+        return redirect('/category');
+            // ->with('success', 'Category created successfully.');
+    }
+
+
+
+    public function action(Request $request, $id)
+    {
+        $request->validate([
+            'category_name' => 'required',
+            'description' => 'required',
+            'photo' => [File::types(['jpg', 'jpeg', 'png', 'gif'])->max(2 * 1024)],
+
+        ]);
+
+        $data = category::find($id);
+        $data->category_name = $request->category_name;
+        $data->description =$request->description;
+        if ($request->file('photo')) {
+            Storage::delete($data->photo);
+            $data->photo = Storage::putFile('gambar', $request->file('photo'));
+        }
+        $data->save();
+        return redirect('/category');
+    }
+
+    
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $data = category::find($id);
+        if ($data->photo) {
+            Storage::delete($data->photo);
+        }
+        category::destroy($id);
+        return redirect('/category');
+    }
+}
---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rules\File;
+use Illuminate\Support\Facades\Storage;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $data['product'] = product::all();
+        return view('product.index', $data);
+    }
+
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validasi = $request->validate([
+            'product_name' => 'required',
+            'price' => 'required',
+            'category_id' => 'required',
+            'description' => 'required',
+            'photo' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:10',
+            'stock' => 'required'
+        ]);
+
+        if ($request->file('photo')) {
+            $validasi['photo'] = $request->file('photo')->store('gambar');
+        }
+
+        product::create($validasi);
+        return redirect('/product');
+        // ->with('success', 'Product created successfully.');
+    }
+
+    public function action(Request $request, $id)
+    {
+        $request->validate([
+            'product_name' => 'required',
+            'price' => 'required',
+            'category_id' => 'required',
+            'description' => 'required',
+            'photo' => [File::types(['jpg', 'jpeg', 'png', 'gif'])->max(2 * 1024)],
+            'stock' => 'required',
+        ]);
+
+        $data = product::find($id);
+        $data->product_name = $request->product_name;
+        $data->price = $request->price;
+        $data->category_id = $request->category_id;
+        $data->description = $request->description;
+        $data->stock = $request->stock;
+        if ($request->file('photo')) {
+            Storage::delete($data->photo);
+            $data->photo = Storage::putFile('gambar', $request->file('photo'));
+        }
+        $data->save();
+        return redirect('/product');
+    }
+
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $data = product::find($id);
+        if ($data->photo) {
+            Storage::delete($data->photo);
+        }
+        product::destroy($id);
+        return redirect('/product');
+    }
+}
